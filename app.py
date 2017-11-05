1dac7b5f5a089b165eca03a0aa0edfeb203ce6ea
Mohamed Rashid <rashivkp@gmail.com>
2017-11-05 13:14:54 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, flash, render_template, request, redirect, url_for
+import os, re, MySQLdb
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
+
+app = Flask(__name__)
+app.secret_key = 'sdkfajl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rationshop:password@localhost/rationshop'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    number = db.Column(db.String(16))
+    address = db.Column(db.String(128))
+    category = db.Column(db.String(10))
+    member_count = db.Column(db.Integer)
+
+    def __repr__(self):
+        return self.name
+
+    #allotments = relationship( 'Allotment', backref=db.backref('users'))
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+    def __repr__(self):
+        return self.name
+
+class Allotment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    user = relationship('User', backref='allotments')
+    item_id = db.Column(db.Integer, ForeignKey('item.id'))
+    item = relationship('Item', backref='allotments')
+    amount = db.Column(db.Float)
+
+
+class AllotmentView(ModelView):
+    form_ajax_refs = {
+            'user': {
+                'fields': ['name'],
+                'page_size': 10
+                },
+            'item': {
+                'fields': ['name'],
+                'page_size': 10
+                }
+            }
+
+admin = Admin(app, name='Rationshop', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Item, db.session))
+admin.add_view(AllotmentView(Allotment, db.session))
+
+if __name__ == '__main__':
+    app.debug = True
+    manager.run()
+
---
+++ print.php
@@ -0,0 +1,9 @@
+hi suhaib
+<?php
+$t =$_GET['temp'];
+if ($t > 30) {
+    echo "Oh such a hot day,, ";
+} else {
+    echo "you cool,, ";
+}
+
